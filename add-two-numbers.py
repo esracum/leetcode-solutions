d3481ef7c3b15863555258e0d1e7ba4d821d8ac0
esracum <91957149+esracum@users.noreply.github.com>
2025-03-13 13:46:02 +0300

    Update add-two-numbers.py

--- add-two-numbers.py
+++ add-two-numbers.py
@@ -7,12 +7,12 @@ class Solution(object):
         while l1 is not None or l2 is not None:
             x = l1.val if l1 is not None else 0
             y = l2.val if l2 is not None else 0
+            
             sum = carry + x + y
             carry = sum // 10
 
             temp.next = ListNode(sum % 10)
-
-
+            
             temp = temp.next
             if l1 is not None: l1 = l1.next
             if l2 is not None: l2 = l2.next
